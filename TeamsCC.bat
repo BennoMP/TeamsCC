c774f2c5364ee60da7a71963d5a0e4094be64533
Benno <61586142+BennoMP@users.noreply.github.com>
2021-02-09 10:19:06 +0100

    v0.2

---
+++ TeamsCC.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Clearing caches. Please be patient...
+cd %appdata%\Microsoft\teams
+del /Q /S blob_storage
+del /Q /S databases
+del /Q /S GPUCache
+del /Q /S IndexedDB
+del /Q /S tmp
+echo ##############################################################################
+echo # The next task might delete settings. Only say "y" if you're having issues. #
+echo ##############################################################################
+del /S "Local Storage"
+echo Now proceeding to launch Microsoft Teams. Please be patient...
+%USERPROFILE%\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
