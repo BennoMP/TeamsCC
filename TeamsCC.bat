e9d7dff4a32beca57c45cefb7acd6317fcedda0e
Benno <61586142+BennoMP@users.noreply.github.com>
2021-02-21 16:05:52 +0100

    Added some pretties and removed a useless function

--- TeamsCC.bat
+++ TeamsCC.bat
@@ -1,18 +1,29 @@
 @echo off
-echo Release 0.3, brought to you by BennoMP
-echo Killing Microsoft Teams processes. Please be patient...
+echo #################################
+echo # Release 0.3, made by BennoMP. #
+echo #################################
+timeout 1 > NUL
+echo ###########################################################
+echo # Killing Microsoft Teams processes. Please be patient... #
+echo ###########################################################
 taskkill /IM Teams.exe /T /F
 timeout 1 > NUL
-echo Now clearing caches. Please be patient...
+echo #############################################
+echo # Now clearing caches. Please be patient... #
+echo #############################################
 cd %appdata%\Microsoft\teams
 del /Q /S blob_storage
+echo ----------                                         (20%)
 del /Q /S databases
+echo --------------------                               (40%)
 del /Q /S GPUCache
+echo ------------------------------                     (60%)
 del /Q /S IndexedDB
+echo ---------------------------------------            (80%)
 del /Q /S tmp
-echo ##############################################################################
-echo # The next task might delete settings. Only say "y" if you're having issues. #
-echo ##############################################################################
-del /S "Local Storage"
-echo Now proceeding to launch Microsoft Teams. Please be patient...
+echo -------------------------------------------------  (100%)
+timeout 1 > NUL
+echo ##################################################################
+echo # Now proceeding to launch Microsoft Teams. Please be patient... #
+echo ##################################################################
 %USERPROFILE%\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
